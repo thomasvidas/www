3506a9e97638c7dc4b33e29951c7dc8d7b3087de
Thomas Vidas <hello@thomasvidas.com>
2022-12-20 21:07:49 -0500

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Thomas Vidas' Website";
+export const SITE_DESCRIPTION = "Thomas Vidas' personal website. It's full of cool things!";
